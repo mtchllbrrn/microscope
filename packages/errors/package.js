da7ee803a43ac7333006b184323a57f374312820
Mitchell Barron <mitchell.stocky@gmail.com>
2015-10-11 16:19:53 -0500

    Revert creating package for error-handling.
    This reverts commit 365b93acd80aacd1b6ed0986cff3788631fac6bb.

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -11,10 +11,3 @@ Package.onUse(function(api) {
   if (api.export)
     api.export('Errors');
 });
-
-Package.onTest(function(api) {
-  api.use('mtchllbrrn:errors', 'client');
-  api.use(['tinytest', 'test-helpers'], 'client');
-
-  api.addFiles('errors_tests.js', 'client');
-});
